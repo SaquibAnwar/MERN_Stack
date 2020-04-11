a64dae93e2aff0e1412b0c14a23f0d87664e71b7
SaquibAnwar <saquibedu@gmail.com>
2020-04-11 15:09:23 +0530

    Adding todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
+import GoalList from './components/GoalList'
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  const goals = [
+    {id: 'g1', text: 'Learn React'},
+    {id: 'g2', text: 'Learn Time Complexitites & Algorithms'},
+    {id: 'g3', text: 'Read Steve Jobs'}
+  ]; 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center><h2>Today's Goals</h2></center>
+      <GoalList goals = {goals}/>
     </div>
   );
-}
+};
 
 export default App;
